ed540de53d6500347debe743127ac773063cd91f
hhcme <2415994048@qq.com>
2023-04-25 23:33:55 +0800

    fix:路由增加状态保护

--- lib/src/route/routes.dart
+++ lib/src/route/routes.dart
@@ -1,41 +1,46 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
-import 'package:flutter_frame_hyc/src/log/log.dart';
+import 'package:flutter_frame_hyc/src/route/log.dart';
 
 /// 使用路由的入口
 class RoutesOfHYC {
+  /// 定时器
+  /// 如果这个定时器存在的话, 说明当前路由操作需要丢弃掉
+  Timer? _timer;
+
+  /// 判断能否跳转路由
+  bool canPush() {
+    if (_timer != null) {
+      routeLog.infoLog('处在路由保护中,本次路由操作丢弃');
+      return false;
+    }
+    return true;
+  }
+
+  /// 开启路由保护状态定时器
+  void runTimer() {
+    if(_timer!=null){
+      routeLog.infoLog('当前已经处在路由保护中');
+      return;
+    }
+
+    routeLog.infoLog('路由保护状态开始');
+    _timer = Timer(const Duration(milliseconds: 500), () {
+      routeLog.infoLog('路由保护状态结束');
+      _timer?.cancel();
+      _timer = null;
+    });
+  }
 
   /// 直接跳转路由
   ///
   /// 跳转命名路由
-  Future<T?>? toNamed<T>(
-      String name, {
-        dynamic arguments,
-        int? id,
-        bool preventDuplicates = true,
-        Map<String, String>? parameters,
-      }) {
-    if (preventDuplicates && page == currentRoute) {
+  Future<T?>? toNamed<T>(String name) {
+    if (canPush()) {
       return null;
     }
-
-    /// 加路由切换时间保护锁
-    if (!IRouteTimeProtect.instance.tryProtect(RouteDirection.forward, page)) {
-      return null;
-    }
-
-    if (parameters != null) {
-      final uri = Uri(path: page, queryParameters: parameters);
-      page = uri.toString();
-    }
-
-    return global(id).currentState?.pushNamed<T>(
-      page,
-      arguments: arguments,
-    );
   }
+
   /// 关闭当前路由
   ///
   /// 一直退出路由, 直到被打断
