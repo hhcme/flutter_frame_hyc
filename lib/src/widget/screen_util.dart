052f14bfddf55cbd566d3c97a566a75399afc7b2
hhcme <2415994048@qq.com>
2023-05-25 23:33:35 +0800

    删除path_provider依赖,全局适配比例改为iphone14比例

--- lib/src/widget/screen_util.dart
+++ lib/src/widget/screen_util.dart
@@ -65,8 +65,9 @@ class _ScreenWidgetState extends State<ScreenWidget> with WidgetsBindingObserver
 /// [_textScaleFactor] 字体的缩放比例
 class ScreenUtil {
   static ScreenUtil _instance = ScreenUtil._();
-  static int width = 1000; //设计稿宽度
-  static int height = 2111; //设计稿高度
+  /// 这里是iphone14的屏幕分辨率
+  static int width = 1170; //设计稿宽度
+  static int height = 2532; //设计稿高度
   static double? _deviceWidth; //实际设备宽度
   static double? _deviceHeight; //实际设备高度
   static double? _textScaleFactor; //字体的缩放比例
